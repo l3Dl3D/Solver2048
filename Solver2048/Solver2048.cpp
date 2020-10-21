a13c94a6911ba4e5ba4e25585e2f7d3eff8110fc
l3D <pupipup33@gmail.com>
2020-10-21 08:04:09 +0300

    Add cache, this commit has a good chance.

--- Solver2048/Solver2048.cpp
+++ Solver2048/Solver2048.cpp
@@ -8,8 +8,20 @@
 #include <random>
 #include <chrono>
 #include <vector>
+#include <unordered_map>
 
 namespace {
+	template <int A, int B>
+	struct get_power
+	{
+		static const long long value = A * get_power<A, B - 1>::value;
+	};
+	template <int A>
+	struct get_power<A, 0>
+	{
+		static const long long value = 1;
+	};
+
 	enum directrion_t {
 		up = 0,
 		right = 1,
@@ -21,14 +33,15 @@ namespace {
 	struct Cell {
 		int x;
 		int y;
+		Cell() : x(-1), y(-1) {}
 		Cell(int _x, int _y) : x(_x), y(_y) {}
 	};
 
 	template<int SIZE>
 	struct Board {
-		std::array<std::array<unsigned int, SIZE>, SIZE> mGrid;
+		std::array<std::array<uint16_t, SIZE>, SIZE> mGrid;
 		unsigned int mScore;
-	
+
 		int move(directrion_t dir) {
 			int moved = 0;
 
@@ -82,7 +95,7 @@ namespace {
 		}
 
 		void transpose() {
-			for(auto i = 0; i < SIZE; i++)
+			for (auto i = 0; i < SIZE; i++)
 				for (auto j = 0; j < i; j++)
 					std::swap(mGrid[i][j], mGrid[j][i]);
 		}
@@ -92,12 +105,12 @@ namespace {
 				std::reverse(row->begin(), row->end());
 		}
 
-		std::vector<Cell> getEmptyCells() const {
-			std::vector<Cell> res;
+		int getEmptyCells(Cell cells[SIZE * SIZE]) const {
+			int res = 0;
 			for (auto i = 0; i < SIZE; i++)
 				for (auto j = 0; j < SIZE; j++) {
 					if (mGrid[i][j] == 0)
-						res.emplace_back(j, i);
+						cells[res++] = { j, i };
 				}
 			return res;
 		}
@@ -111,8 +124,35 @@ namespace {
 				std::cout << "\n";
 			}
 		}
-	};
 
+		bool operator==(const Board<SIZE>& other) const {
+			return other.mScore == this->mScore && other.mGrid == this->mGrid;
+		}
+
+		uint16_t getMax() const {
+			uint16_t max = 0;
+			for (auto row : this->mGrid)
+				for (auto cell : row)
+					max = std::max(max, cell);
+			return max;
+		}
+	};
+}
+
+namespace std {
+	template<>
+	struct hash<std::pair<Board<4>, int>> {
+		size_t operator()(const std::pair<Board<4>, int>& board) const {
+			size_t res = 0;
+			for (auto i = 0; i < 4; i++)
+				for (auto j = 0; j < 4; j++)
+					res ^= std::hash<unsigned short>()(board.first.mGrid[j][i] + i + j);
+			return std::hash<int>()(board.first.mScore) ^ std::hash<int>()(board.second) ^ res;
+		}
+	};
+}
+
+namespace {
 	template<int SIZE>
 	class GameManager {
 	private:
@@ -130,8 +170,9 @@ namespace {
 
 		void putRandomTile() {
 			// Gather all empty cells.
-			auto emptyCells = mBoard.getEmptyCells();
-			auto size = emptyCells.size();
+			Cell emptyCells[SIZE * SIZE];
+			int emptyCellsSize = mBoard.getEmptyCells(emptyCells);
+			auto size = emptyCellsSize;
 			if (size == 0)
 				return;
 
@@ -142,6 +183,7 @@ namespace {
 			// Put random tiles (4 if 0, else 2)
 			std::uniform_int_distribution<int> dist4(0, 9);
 			bool put4 = dist4(mGen) == 0;
+			if (mBoard.mGrid[cell.y][cell.x]) exit(-1);
 			mBoard.mGrid[cell.y][cell.x] = put4 ? 2 : 4;
 		}
 
@@ -175,51 +217,106 @@ namespace {
 	};
 
 	template<int SIZE>
-	double calcBoardScore(const Board<SIZE>& board) {
-		double res = static_cast<double>(board.mScore);
+	double calcBoardScoreInternal(const Board<SIZE>& board) {
+		double res = 0;
+		res += static_cast<double>(board.mScore);
+		res += static_cast<double>(board.getMax()) * 4;
 
-		unsigned int max = 0;
-		for (auto row : board.mGrid)
-			for(auto cell : row)
-				max = std::max(max, cell);
+		res += board.mGrid[0][0] * 40;
+		res += board.mGrid[0][1] * 29;
+		res += board.mGrid[0][2] * 27;
+		res += board.mGrid[0][3] * 25;
+
+		res += board.mGrid[1][0] * 29;
+		res += board.mGrid[1][1] * 18;
+		res += board.mGrid[1][2] * 16;
+		res += board.mGrid[1][3] * 14;
 
 		/*
-		if (board.mGrid[0][0] == max)
-			res += max * 64;
+		res += board.mGrid[2][0] * get_power<2, 7>::value;
+		res += board.mGrid[2][1] * get_power<2, 5>::value;
+		res += board.mGrid[2][2] * get_power<2, 3>::value;
+		res += board.mGrid[2][3] * get_power<2, 1>::value;
 		*/
 
-		res += board.mGrid[0][0] * std::pow(3, 4);
-		res += board.mGrid[0][1] * std::pow(3, 3);
-		res += board.mGrid[0][2] * std::pow(3, 2);
-		res += board.mGrid[0][3] * std::pow(3, 1);
+		double smooth = 0;
+		double empty = 0;
+		for (auto y = 0; y < SIZE; y++) {
+			for (auto x = 0; x < SIZE; x++) {
+				if (y > 0 && board.mGrid[y][x] == board.mGrid[y - 1][x]) smooth++;
+				if (x > 0 && board.mGrid[y][x] == board.mGrid[y][x - 1]) smooth++;
+				if (board.mGrid[y][x] == 0) empty++;
+			}
+		}
+		res += smooth * 8 + empty * 8;
+
 		return res;
 	}
 
 	template<int SIZE>
-	double calcScore(const Board<SIZE>& board, int depth) {
-		if(depth == 0)
-			return calcBoardScore<SIZE>(board);
-
+	double calcBoardScore(const Board<SIZE>& board) {
 		double res = 0;
+		auto boardCopy = board;
+		for (auto i = 0; i < 4; i++) {
+			boardCopy.transpose();
+			res = std::max(res, calcBoardScoreInternal<SIZE>(boardCopy));
+			boardCopy.flip();
+			res = std::max(res, calcBoardScoreInternal<SIZE>(boardCopy));
+		}
+		return res;
+	}
+
+	template<int SIZE>
+	double calcScore(const Board<SIZE>& board, int depth, int& bestMoveOut, int& stats,
+		std::unordered_map<std::pair<Board<SIZE>, int>, std::pair<double, int>>& cache, int numOfFour = 0) {
+		if (depth == 0) {
+			stats++;
+			return calcBoardScore<SIZE>(board);
+		}
+
+		auto p = std::make_pair(board, depth);
+		if (cache.count(p) == 1) {
+			auto cached = cache[p];
+			bestMoveOut = cached.second;
+			return cached.first;
+		}
+
+		double bestScore = 0;
+		int bestMove = -1;
+
 		for (int dir = 0; dir < max_dir; dir++) {
 			Board<SIZE> boardCopy = board;
 			if (boardCopy.move((directrion_t)dir) == 0)
 				continue;
 
-			auto emptyCells = boardCopy.getEmptyCells();
+			Cell emptyCells[SIZE * SIZE];
+			int emptyCellsSize = boardCopy.getEmptyCells(emptyCells);
+			double currScore = 0;
 
-			double currRes = 0;
-			for (auto cell : emptyCells) {
-				boardCopy.mGrid[cell.y][cell.x] = 2;
-				res += calcScore(boardCopy, depth - 1) / emptyCells.size() * 0.9;
-				boardCopy.mGrid[cell.y][cell.x] = 4;
-				res += calcScore(boardCopy, depth - 1) / emptyCells.size() * 0.1;
+			for (auto cellIndex = 0; cellIndex < emptyCellsSize; cellIndex++) {
+				boardCopy.mGrid[emptyCells[cellIndex].y][emptyCells[cellIndex].x] = 2;
+				currScore += calcScore(boardCopy, depth - 1, bestMoveOut, stats, cache, numOfFour) / emptyCellsSize * 0.9;
+				if (numOfFour < 2) {
+					boardCopy.mGrid[emptyCells[cellIndex].y][emptyCells[cellIndex].x] = 4;
+					currScore += calcScore(boardCopy, depth - 1, bestMoveOut, stats, cache, numOfFour + 1) / emptyCellsSize * 0.1;
+				}
+				boardCopy.mGrid[emptyCells[cellIndex].y][emptyCells[cellIndex].x] = 0;
 			}
 
-			res = std::max(res, currRes);
+			if (currScore > bestScore) {
+				bestScore = currScore;
+				bestMove = dir;
+			}
 		}
 
-		return res;
+		if (bestMove == -1) {
+			std::mt19937 gen((unsigned)std::chrono::system_clock::now().time_since_epoch().count());
+			std::uniform_int_distribution<int> dist(0, 3);
+			bestMove = dist(gen);
+		}
+		bestMoveOut = bestMove;
+		cache[p] = std::make_pair(bestScore, bestMove);
+		return bestScore;
 	}
 
 	class Player {
@@ -227,39 +324,33 @@ namespace {
 		static const int SIZE = 4;
 		GameManager<SIZE> mGM;
 
-		void startGame() {
+		int startGame() {
 			while (mGM.movesAvailable()) {
 				int bestMove = -1;
-				double bestScore = -1;
-				int dir = 0;
-				for (; dir < 4; dir++) {
-					auto board = mGM.getBoard();
-					if(board.move((directrion_t)dir) == 0)
-						continue;
-					double currScore = calcScore<SIZE>(board, 2);
-					if (currScore > bestScore) {
-						bestScore = currScore;
-						bestMove = dir;
-					}
-				}
-
+				auto board = mGM.getBoard();
+				int stats = 0;
+				int depth = 3;
+				std::unordered_map<std::pair<Board<SIZE>, int>, std::pair<double, int>> cache;
+				double currScore = calcScore<SIZE>(board, depth, bestMove, stats, cache);
+				mGM.dump();
+				std::cout << "Stats: " << stats << "\n";
+				std::cout << "Move: " << bestMove << "\n\n";
+				
 				mGM.move((directrion_t)bestMove);
 			}
+
+			return mGM.getBoard().getMax();
+			return mGM.getBoard().mScore;
 		}
 	};
 }
 
 int main()
 {
-	int avg = 0;
-	for (auto i = 0; i < 100; i++) {
+	for (auto i = 0; i < 1; i++) {
 		Player player;
-		player.startGame();
-		player.mGM.dump();
-		avg += player.mGM.getBoard().mScore;
+		std::cout << player.startGame() << "\n";
 	}
-	avg /= 100;
-	std::cout << avg << "\n";
 
 	return 0;
 }
